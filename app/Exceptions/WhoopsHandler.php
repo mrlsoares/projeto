588801f2231832777ede18679b95dad8fad64146
Marcos Soares <mrlsoares@gmail.com>
2015-08-07 14:29:32 -0300

    instalação debug woops

---
+++ app/Exceptions/WhoopsHandler.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mrlsoares
+ * Date: 07/08/15
+ * Time: 13:11
+ */
+
+namespace Projeto\Exceptions;
+
+
+use Exception;
+use Illuminate\Http\Response;
+use Projeto\Exceptions\Handler as BaseExceptionHandler;
+
+class WhoopsHandler extends BaseExceptionHandler {
+
+	/**
+	 * Render an exception into a response.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param \Exception $e
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function render($request, Exception $e) {
+		$whoops = new \Whoops\Run;
+
+		if ($request->ajax())
+		{
+			$whoops->pushHandler(new \Whoops\Handler\JsonResponseHandler());
+		}
+		else
+		{
+			$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+		}
+
+		return new Response($whoops->handleException($e), $e->getStatusCode(), $e->getHeaders());
+	}
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -38,6 +38,7 @@ $app->singleton(
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
+    Projeto\Exceptions\WhoopsHander::class,
     Projeto\Exceptions\Handler::class
 );
 
